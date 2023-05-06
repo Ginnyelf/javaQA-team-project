3a5127b89c8409ff1983d1849fb843f1fd5f6f1c
Ginnyelf <ginnyelf0801@gmail.com>
2023-05-06 18:44:26 +0300

    Ginny commit Saving

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -21,7 +21,7 @@ public class SavingAccountTest {
     }
 
     @Test
-    public void increaseNegativeBalance() {
+    public void WhenInitialBalanceIsNegative() {
         SavingAccount account = new SavingAccount(
                 -1_000,
                 1_000,
@@ -49,6 +49,21 @@ public class SavingAccountTest {
     }
 
     @Test
+    public void WhenAfterAddOverMaxLimit() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(10000);
+
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+
+    @Test
     public void decreaseBalanceLessMinLimit() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -61,35 +76,49 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2000, account.getBalance());
     }
-
-
     @Test
-    public void decreaseZeroBalance() { ///// ???????????????????
+    public void ShouldPay() {
         SavingAccount account = new SavingAccount(
-                0,
+                2_000,
                 1_000,
                 10_000,
                 5
         );
 
-        account.pay(3_000);
+        account.pay(1_000);
 
-        Assertions.assertEquals(0, account.getBalance());
+        Assertions.assertEquals(1000, account.getBalance());
     }
 
+
     @Test
-    public void percentOverHundred() {
+    public void shouldCalculateSumOfPercent() {
         SavingAccount account = new SavingAccount(
 
-                2_000,
-                1_000,
+                200,
+                200,
                 10_000,
-                150
+                15
         );
 
         account.yearChange();
 
-        Assertions.assertEquals(0, account.yearChange());
+        Assertions.assertEquals(30, account.yearChange());
+    }
+
+    @Test
+    public void WhenRateIsNegative() {
+        SavingAccount account = new SavingAccount(
+
+                200,
+                200,
+                10_000,
+                -15
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(200, account.yearChange());
     }
 
 }
